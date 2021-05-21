03efaabf6caf7c227d7b96249ceae050af14a319
luizportel4 <luizfjportela@gmail.com>
2021-05-20 21:02:06 -0300

    Add exercise 09

---
+++ bloco_4/dia_2/exercise-09.js
@@ -0,0 +1,10 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+
+let array = [];
+let aux = [];
+for (let index = 0; index < 25; index += 1) {
+  array.push(index);
+  console.log(array[index]/2);
+
+}
+
