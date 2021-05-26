88482212d26dbef8ab0db233f7b5498109594e75
LuizFJP <luizfjportela@gmail.com>
2021-05-26 19:28:23 -0300

    fixation exercise 03 querySelector and querySelectorAll

---
+++ bloco_5/script.js
@@ -0,0 +1,18 @@
+
+
+const firstTitleOrange = document.querySelectorAll(".emergency-tasks h3")[0].style.background = "#FB5607";
+
+const secondTitleOrange = document.querySelectorAll(".emergency-tasks h3")[1].style.background = "#FB5607";
+
+const firstTitlePink = document.querySelectorAll(".no-emergency-tasks h3")[0].style.background = "#FF006E";
+
+const secondTitlePink = document.querySelectorAll(".no-emergency-tasks h3")[1].style.background = "#FF006E";
+
+const firstBackgroundYellow = document.querySelector(".emergency-tasks div").style.background = "#ffbe0b";
+
+const secondBackgroundYellow = document.querySelectorAll(".emergency-tasks div")[1].style.background = "#ffbe0b";
+
+const firstBackgroundBlue = document.querySelector(".no-emergency-tasks div").style.background = "#3A86FF";
+
+const secondBackgroundBlue = document.querySelectorAll(".no-emergency-tasks div")[1].style.background = "#3A86FF";
+
