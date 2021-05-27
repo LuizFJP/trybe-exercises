80a67107efbe8b7181f7a3ad82e0db1850c39726
LuizFJP <luizfjportela@gmail.com>
2021-05-27 18:54:35 -0300

    fixation exercise 01

---
+++ bloco_5/dia_2/script.js
@@ -0,0 +1,16 @@
+const whereYouAre = document.getElementById("elementoOndeVoceEsta");
+
+const dad = whereYouAre.parentElement.style.color = "#3A86FF";
+
+const firstSonOfSon = whereYouAre.firstElementChild.innerText = "Some text";
+
+const firstSon = dad.firstElementChild;
+
+const firstSonByWhere = whereYouAre.parentElement.parentElement;
+
+const text = whereYouAre.parentElement.parentElement;
+
+const thirdElement = whereYouAre.nextElementSibling;
+
+const thirdElementByDad = dad.lastElementChild.previousElementSibling;
+
