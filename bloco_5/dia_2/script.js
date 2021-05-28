842b75ace23e9f2806197c6757148d9040a9d59e
LuizFJP <luizfjportela@gmail.com>
2021-05-28 13:26:31 -0300

    Crate e simbling for 'whereYouAre'

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -1,6 +1,6 @@
 const whereYouAre = document.getElementById("elementoOndeVoceEsta");
 
-const dad = whereYouAre.parentElement.style.color = "#3A86FF";
+const dad = whereYouAre.parentElement;dad.style.color = "#3A86FF";
 
 const firstSonOfSon = whereYouAre.firstElementChild.innerText = "Some text";
 
@@ -13,4 +13,11 @@ const text = whereYouAre.parentElement.parentElement;
 const thirdElement = whereYouAre.nextElementSibling;
 
 const thirdElementByDad = dad.lastElementChild.previousElementSibling;
+
+//crie um irmão p/ 'elementoOndeVoceEsta'
+const father = getElementById('pai');
+const whereYouAreBrother = document.createElement('div');
+whereYouAreBrother.id = "whereYouAreBrother"
+father.appendChild(whereYouAreBrother);
+
 
