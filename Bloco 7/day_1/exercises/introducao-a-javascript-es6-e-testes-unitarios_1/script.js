c4fe162ecfd9cfe9df858c2244992089c062e1e2
LuizFJP <luizfjportela@gmail.com>
2021-06-17 18:50:59 -0300

    part 01: exercise 02

--- Bloco 7/day_1/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ Bloco 7/day_1/exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -1,13 +1,5 @@
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  //console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
-}
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-testingScope(true);
+oddsAndEvens.sort((a, b) => a - b);
+
+console.log(oddsAndEvens);
