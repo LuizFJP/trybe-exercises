c15a9149a2769e2cac4ca1fedc1b1cf6542e16eb
LuizFJP <luizfjportela@gmail.com>
2021-07-20 19:22:38 -0300

    fixation exercises react

---
+++ Bloco 11/Dia 1/fixation-exercise/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Me extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Luiz Portela</h1>
+        <p>18 anos, Paraense e apaixonado por aprender.</p>
+      </div>
+    )}
+}
+
+export default Me;
