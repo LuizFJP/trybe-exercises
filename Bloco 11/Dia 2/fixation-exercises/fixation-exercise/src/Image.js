caaac53cd24eec907937c197c5865e7f8aa27633
LuizFJP <luizfjportela@gmail.com>
2021-07-21 16:15:37 -0300

    fixation exercises01 react

---
+++ Bloco 11/Dia 2/fixation-exercises/fixation-exercise/src/Image.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class Image extends React.Component {
+  render() {
+    return <img src={`${this.props.source}.jpg`} alt={this.props.alternativeText} />;
+  }
+}
+
+export default Image;
