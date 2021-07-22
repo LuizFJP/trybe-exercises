39a860441eb852416b8740f162ecc7a33ed43a17
LuizFJP <luizfjportela@gmail.com>
2021-07-21 22:20:24 -0300

    fixation exercise 01

--- Bloco 11/Dia 1/fixation-exercise/src/App.js
+++ Bloco 11/Dia 1/fixation-exercise/src/App.js
@@ -3,8 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Component from './Component';
 
+  function message(value) {
+    return <h1>{ value }</h1>
+  }
+  const testJSX = 'Hello, JSX';
 function App() {
-  return <Component/>
+  return( 
+  message(testJSX)
+  );
 }
 
 export default App;
