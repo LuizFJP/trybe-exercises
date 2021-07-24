9dd42dc0f4d5879b56415aaba269d7ca5126af35
LuizFJP <luizfjportela@gmail.com>
2021-07-24 01:50:20 -0300

    fixation exercises 05 to 07

--- Bloco 12/Dia 1/fixation-exercises/src/App.js
+++ Bloco 12/Dia 1/fixation-exercises/src/App.js
@@ -5,24 +5,43 @@ import './App.css';
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      numOfClickIm: 0
+    }
     this.im = this.im.bind(this);
+    this.state = {
+      numOfClickLearning: 0
+    }
     this.learning = this.learning.bind(this);
+    this.state = {
+      numOfClickReact: 0
+    }
     this.react = this.react.bind(this);
   }
 
-  im() {
-    console.log("I'm");
+  im = () => {
+    // console.log("I'm");
+    this.setState((beforeState, _props) => ({
+      numOfClickIm: beforeState.numOfClicksIm + 1
+    }))
   }
 
-  learning() {
-    console.log('learning');
+  learning = () => {
+    // console.log('learning');
+    this.learning((beforeState, _props) => ({
+      numOfClickLearning: beforeState.numOfClickLearning + 1
+    }))
   }
 
-  react() {
-    console.log('React');
+  react = () => {
+    // console.log('React');
+    this.setState((beforeState, _props) => ({
+      numOfClickReact: beforeState.numOfClickReact + 1
+    }))
   }
 
   render () {
+    console.log(this);
     return(
       <div>
         <button onClick = {this.im}>I'm</button>
