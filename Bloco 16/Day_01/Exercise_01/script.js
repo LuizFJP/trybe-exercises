822ed32e6dd48d68c2929fcee1a6f502d8965d28
LuizFJP <luizfjportela@gmail.com>
2021-08-23 18:47:21 -0300

    requirement 01 exercise 02

--- Bloco 16/Day_01/Exercise_01/script.js
+++ Bloco 16/Day_01/Exercise_01/script.js
@@ -3,4 +3,25 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
-const store = Redux.createStore();
+const NEXT_COLOR = 'NEXT_COLOR';
+const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
+
+const nextColor = (color) => ({
+  type: NEXT_COLOR,
+});
+
+const previousColor = (color) => ({
+  type: PREVIOUS_COLOR,
+});
+
+const reducer = (state = 'default', action) => {
+  switch(action.type) {
+    case NEXT_COLOR:
+      return {state};
+    case PREVIOUS_COLOR:
+      return {}
+  }
+  
+}
+
+const store = Redux.createStore(reducer);
