125a6718d8ba861270c4089f04c0adf7193c33f2
LuizFJP <luizfjportela@gmail.com>
2021-10-14 17:40:10 -0300

    exercise 07 and 08

--- Bloco 19/Day_2/exercises_day.sql
+++ Bloco 19/Day_2/exercises_day.sql
@@ -3,4 +3,6 @@ SELECT 1 AS 'Number One', 2 AS 'Number Two', 3 AS 'Number Three';
 SELECT 10 + 15;
 SELECT (10 + 15) * 2;
 SELECT * FROM Scientists;
-SELECT Projects.Name, Hours FROM Projects
+SELECT Projects.Name, Hours FROM Projects;
+SELECT Name FROM Scientists ORDER BY Scientists.Name ASC; 
+SELECT Name FROM Projects ORDER BY Name DESC;
