7b2ac241015e51f5590389c32c36c13f180bb03e
LuizFJP <luizfjportela@gmail.com>
2021-10-14 17:44:28 -0300

    exercise 09

--- Bloco 19/Day_2/exercises_day.sql
+++ Bloco 19/Day_2/exercises_day.sql
@@ -6,3 +6,4 @@ SELECT * FROM Scientists;
 SELECT Projects.Name, Hours FROM Projects;
 SELECT Name FROM Scientists ORDER BY Scientists.Name ASC; 
 SELECT Name FROM Projects ORDER BY Name DESC;
+SELECT CONCAT('O projeto ', Name, ' precisou de ' ,Hours, ' horas para ser concluído') FROM Projects;
