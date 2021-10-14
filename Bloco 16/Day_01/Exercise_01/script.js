e0961a48895333198ac907ad187ee96f66fc440c
LuizFJP <luizfjportela@gmail.com>
2021-10-14 17:54:43 -0300

    exercise 10 fixed and 12

--- Bloco 16/Day_01/Exercise_01/script.js
+++ Bloco 16/Day_01/Exercise_01/script.js
@@ -6,22 +6,43 @@ const ESTADO_INICIAL = {
 const NEXT_COLOR = 'NEXT_COLOR';
 const PREVIOUS_COLOR = 'PREVIOUS_COLOR';
 
-const nextColor = (color) => ({
+const nextColor = () => ({
   type: NEXT_COLOR,
 });
 
-const previousColor = (color) => ({
+const previousColor = () => ({
   type: PREVIOUS_COLOR,
 });
 
-const reducer = (state = 'default', action) => {
+const reducer = (state = ESTADO_INICIAL, action) => {
   switch(action.type) {
     case NEXT_COLOR:
-      return {state};
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      };
     case PREVIOUS_COLOR:
-      return {}
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      };
+    default:
+      return state;
   }
-  
-}
+};
 
 const store = Redux.createStore(reducer);
+
+document.querySelector('#previous').addEventListener('click', () => {
+  store.dispatch(previousColor());
+})
+
+document.querySelector('#next').addEventListener('click', () => {
+  store.dispatch(nextColor());
+})
+
+store.subscribe(() => {
+  const { colors, index } = store.getState();
+  document.querySelector('#value').innerHTML = colors[index];
+  document.querySelector('#container').style.backgroundColor = colors[index];
+});
