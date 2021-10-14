866060f3eee06d6204e78aa7fbe3a9ec1869ad8d
LuizFJP <luizfjportela@gmail.com>
2021-10-14 18:03:46 -0300

    exercise 13 to 15

--- Bloco 19/Day_2/exercises_day.sql
+++ Bloco 19/Day_2/exercises_day.sql
@@ -7,5 +7,11 @@ SELECT Projects.Name, Hours FROM Projects;
 SELECT Name FROM Scientists ORDER BY Scientists.Name ASC; 
 SELECT Name FROM Projects ORDER BY Name DESC;
 SELECT CONCAT('O projeto ', Name, ' precisou de ' ,Hours, ' horas para ser concluído') FROM Projects;
-SELECT Name, Hours FROM Projects ORDER BY Hours DESC;
+SELECT Name, Hours FROM Projects ORDER BY Hours DESC LIMIT 3;
 SELECT DISTINCT Scientist FROM AssignedTo;
+SELECT Name, Hours FROM Projects ORDER BY Hours DESC LIMIT 1;
+SELECT Name FROM Projects ORDER BY Hours ASC LIMIT 100 OFFSET 1;
+SELECT * FROM Projects ORDER BY Hours ASC LIMIT 5;
+SELECT CONCAT('Existem ', COUNT(SSN),' cientistas na tabela Scientists.')
+AS 'Número de cientistas'
+FROM Scientists;
