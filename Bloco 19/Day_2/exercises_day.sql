8aafcf3dca6b0e926174edfe4ab767fcc9b11e2c
LuizFJP <luizfjportela@gmail.com>
2021-10-14 17:38:01 -0300

    exercise 06

--- Bloco 19/Day_2/exercises_day.sql
+++ Bloco 19/Day_2/exercises_day.sql
@@ -2,4 +2,5 @@ SELECT "This is SQL Exercise, Practice and Solution";
 SELECT 1 AS 'Number One', 2 AS 'Number Two', 3 AS 'Number Three';
 SELECT 10 + 15;
 SELECT (10 + 15) * 2;
-SELECT 
+SELECT * FROM Scientists;
+SELECT Projects.Name, Hours FROM Projects
