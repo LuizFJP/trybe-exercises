3c9ac1c9b5c139b5747e874ceb67ad6546129a60
LuizFJP <luizfjportela@gmail.com>
2021-10-14 18:28:57 -0300

    exercise 5 BONUS

--- Bloco 19/Day_2/exercise_bonusDay.sql
+++ Bloco 19/Day_2/exercise_bonusDay.sql
@@ -2,3 +2,4 @@ SELECT Piece, Price FROM Provides where Provider = "RBT";
 SELECT * FROM Provides ORDER BY Price DESC LIMIT 5;
 SELECT Provider, Price FROM Provides ORDER BY Price LIMIT 4 OFFSET 2;
 SELECT * FROM Provides where Provider = "HAL" ORDER BY Price DESC;
+SELECT COUNT(Provider) FROM Provides where Piece = 1; 
