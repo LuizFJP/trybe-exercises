8f25900d1d9e43ea497e104e095649b4add08ec3
LuizFJP <luizfjportela@gmail.com>
2021-10-15 15:52:17 -0300

    fixation where exercise 3

--- Bloco 19/Day_3/Exercise_day_where.sql
+++ Bloco 19/Day_3/Exercise_day_where.sql
@@ -3,4 +3,11 @@ WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
 SELECT first_name FROM customer
 WHERE active IS FALSE
 AND store_id = 2
-AND first_name NOT LIKE 'KENNETH%';
+AND first_name NOT LIKE 'KENNETH%'
+ORDER BY first_name;
+SELECT title, description, release_year, replacement_cost
+FROM film
+WHERE NOT rating = 'NC-17'
+AND replacement_cost >= 18 
+ORDER BY replacement_cost DESC,
+title ASC;
