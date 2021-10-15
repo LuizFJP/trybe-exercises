5d16d4b7b959fffab4c4c94a5a29de3f10259eb2
LuizFJP <luizfjportela@gmail.com>
2021-10-15 15:39:48 -0300

    fixation where exercise 2

--- Bloco 19/Day_3/Exercise_day_where.sql
+++ Bloco 19/Day_3/Exercise_day_where.sql
@@ -1,2 +1,6 @@
 SELECT * FROM customer
 WHERE email = 'LEONARD.SCHOFIELD@sakilacustomer.org';
+SELECT first_name FROM customer
+WHERE active IS FALSE
+AND store_id = 2
+AND first_name NOT LIKE 'KENNETH%';
