fbbcb9a615fe01f0da7463dafb2fb9e7cb3f886f
LuizFJP <luizfjportela@gmail.com>
2021-10-17 13:29:46 -0300

    fixation between in exercise 05

--- Bloco 19/Day_3/Exercise_day_between_in.sql
+++ Bloco 19/Day_3/Exercise_day_between_in.sql
@@ -13,3 +13,6 @@ SELECT title, release_year, rental_duration
 FROM film
 WHERE rental_duration BETWEEN 3 AND 6
 ORDER BY rental_duration DESC, title ASC;
+SELECT title, rating FROM film
+WHERE rating IN ('G','PG','PG-13')
+ORDER BY title ASC LIMIT 500;
