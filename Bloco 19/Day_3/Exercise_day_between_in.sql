a1d7bff6ad8df970ae3a7a816ae7023ad0e5bf42
LuizFJP <luizfjportela@gmail.com>
2021-10-17 13:04:14 -0300

    fixation between in exercise 03

--- Bloco 19/Day_3/Exercise_day_between_in.sql
+++ Bloco 19/Day_3/Exercise_day_between_in.sql
@@ -7,3 +7,5 @@ WHERE last_name IN ('hicks','crawford','henry','boyd','mason','morales','kennedy
 SELECT email, address_id FROM customer
 WHERE address_id BETWEEN 172 AND 176
 ORDER BY email ASC;
+SELECT COUNT(payment_id) FROM payment
+WHERE payment_date BETWEEN '2005/05/01' AND '2005/08/01';
