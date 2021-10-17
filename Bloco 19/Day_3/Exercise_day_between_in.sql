430b7651d5c9d0e174dcf0a23a6afe87a4c65c0b
LuizFJP <luizfjportela@gmail.com>
2021-10-17 12:54:22 -0300

    fixation between in exercise 01

--- Bloco 19/Day_3/Exercise_day_between_in.sql
+++ Bloco 19/Day_3/Exercise_day_between_in.sql
@@ -1,4 +1,7 @@
 SELECT * FROM payment
 WHERE customer_id IN (269, 239, 126, 399, 142);
-SELECT * FROM address
-WHERE district IN ('QLD', 'Nagasaki', 'Attika', 'Mandalay', 'Nantou', 'Texas');
+SELECT * FROM address;
+SELECT CONCAT(first_name, last_name) AS Nome, email
+FROM customer
+WHERE last_name IN ('hicks','crawford','henry','boyd','mason','morales','kennedy') 
+  
