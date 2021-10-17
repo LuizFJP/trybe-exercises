1ba49477dd5137022d3ab904304c40a0b37ae39f
LuizFJP <luizfjportela@gmail.com>
2021-10-17 13:23:28 -0300

    fixation between in exercise 04

--- Bloco 19/Day_3/Exercise_day_between_in.sql
+++ Bloco 19/Day_3/Exercise_day_between_in.sql
@@ -9,3 +9,7 @@ WHERE address_id BETWEEN 172 AND 176
 ORDER BY email ASC;
 SELECT COUNT(payment_id) FROM payment
 WHERE payment_date BETWEEN '2005/05/01' AND '2005/08/01';
+SELECT title, release_year, rental_duration
+FROM film
+WHERE rental_duration BETWEEN 3 AND 6
+ORDER BY rental_duration DESC, title ASC;
