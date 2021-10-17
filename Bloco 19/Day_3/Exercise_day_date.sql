e891eb6ae5f79862e34be9b1794b7b3b88524845
LuizFJP <luizfjportela@gmail.com>
2021-10-17 13:48:36 -0300

    fixation date  exercise 02

--- Bloco 19/Day_3/Exercise_day_date.sql
+++ Bloco 19/Day_3/Exercise_day_date.sql
@@ -1,2 +1,4 @@
 SELECT COUNT(rental_id) FROM rental
 WHERE DATE(return_date) LIKE '2005-05-25%';
+SELECT COUNT(payment_id) FROM payment
+WHERE DATE(payment_date) BETWEEN '2005-07-01' AND '2005-08-22';
