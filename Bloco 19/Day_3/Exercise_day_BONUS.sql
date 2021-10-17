489641a3d1341a0d001559be4a0af944323a9211
LuizFJP <luizfjportela@gmail.com>
2021-10-17 16:39:11 -0300

    exercise bonus day 06

--- Bloco 19/Day_3/Exercise_day_BONUS.sql
+++ Bloco 19/Day_3/Exercise_day_BONUS.sql
@@ -10,3 +10,5 @@ SELECT COUNT(Scientist) FROM AssignedTo
 WHERE Project IN ('AeH3','Ast3','Che1');
 SELECT * FROM Projects
 WHERE Hours > 500;
+SELECT * FROM Projects
+WHERE Hours > 250 AND Hours < 800;
