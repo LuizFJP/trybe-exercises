6e51aecadc1aaf31b091dbbedf131e3dfa645001
LuizFJP <luizfjportela@gmail.com>
2021-10-17 13:01:04 -0300

    fixation between in exercise 02

--- Bloco 19/Day_3/Exercise_day_between_in.sql
+++ Bloco 19/Day_3/Exercise_day_between_in.sql
@@ -1,7 +1,9 @@
 SELECT * FROM payment
 WHERE customer_id IN (269, 239, 126, 399, 142);
 SELECT * FROM address;
-SELECT CONCAT(first_name, last_name) AS Nome, email
+SELECT CONCAT(first_name, ' ' ,last_name) AS Nome, email
 FROM customer
-WHERE last_name IN ('hicks','crawford','henry','boyd','mason','morales','kennedy') 
-  
+WHERE last_name IN ('hicks','crawford','henry','boyd','mason','morales','kennedy'); 
+SELECT email, address_id FROM customer
+WHERE address_id BETWEEN 172 AND 176
+ORDER BY email ASC;
