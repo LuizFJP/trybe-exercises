402da0ff60ca2e1c298235f8bd1ca7447676b0fc
LuizFJP <luizfjportela@gmail.com>
2021-10-18 16:28:35 -0300

    fixation exercise insert into 03

--- Bloco 19/Day_4/fixationExercise_insertInto.sql
+++ Bloco 19/Day_4/fixationExercise_insertInto.sql
@@ -4,3 +4,7 @@ INSERT INTO sakila.staff (first_name, last_name, address_id, store_id, `active`,
 VALUES 
 ('Bruno', 'Ramos', 3, 1, 1, 'brunoRamos@'),
 ('Maria', 'Angelica', 4, 2, 1, 'mariacraque');
+INSERT INTO sakila.actor (first_name, last_name)
+SELECT first_name, last_name FROM customer
+LIMIT 5;
+SELECT * FROM category;
