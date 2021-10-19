151fbeaf7882a48ccf12506a8bc3a94d80f585bf
LuizFJP <luizfjportela@gmail.com>
2021-10-18 22:23:41 -0300

    fixation exercise delete 03

--- Bloco 19/Day_4/fixationExercise_delete.sql
+++ Bloco 19/Day_4/fixationExercise_delete.sql
@@ -6,3 +6,6 @@ DELETE FROM film_actor
 WHERE actor_id IN (8, 103, 181);
 DELETE FROM actor
 WHERE first_name = 'MATTHEW';
+DELETE FROM film_text
+WHERE `description` LIKE '%saga%'; 
+SELECT * FROM film_text;
