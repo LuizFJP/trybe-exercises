3e9f7460b603d3283b7f35286c1f8ce351f69fe2
LuizFJP <luizfjportela@gmail.com>
2021-10-18 21:27:28 -0300

    fixation exercise update 03

--- Bloco 19/Day_4/fixationExercise_update.sql
+++ Bloco 19/Day_4/fixationExercise_update.sql
@@ -4,3 +4,8 @@ WHERE first_name = 'JULIA';
 UPDATE category
 SET name = 'Science Fiction'
 WHERE name = 'Sci-Fi';
+UPDATE film
+SET rental_rate = 25
+WHERE `length` > 100
+AND rating IN ('G','PG','PG-13')
+AND replacement_cost > 20; 
