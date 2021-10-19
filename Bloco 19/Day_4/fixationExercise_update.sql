d25eb236e58ea57a62bfdd2aa8f615506590db9c
LuizFJP <luizfjportela@gmail.com>
2021-10-18 21:52:34 -0300

    fixation exercise update 04

--- Bloco 19/Day_4/fixationExercise_update.sql
+++ Bloco 19/Day_4/fixationExercise_update.sql
@@ -8,4 +8,11 @@ UPDATE film
 SET rental_rate = 25
 WHERE `length` > 100
 AND rating IN ('G','PG','PG-13')
-AND replacement_cost > 20; 
+AND replacement_cost > 20;
+UPDATE film
+SET rental_rate = (
+CASE
+WHEN `length` IN (0, 100) THEN 10
+WHEN `length` > 100 THEN 20
+ELSE rental_rate
+END);
