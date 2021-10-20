eef5214de209c1a6047b93c6cdb4b8b59809d55a
LuizFJP <luizfjportela@gmail.com>
2021-10-20 18:52:17 -0300

    exercise 01 and 02

---
+++ Bloco 20/day_01/fixation_exercise03.sql
@@ -0,0 +1,5 @@
+-- Monte uma query que gere um valor entre 15 e 20 .
+SELECT ROUND(15 + (RAND() * 5));
+
+-- Monte uma query que exiba o valor arredondado de 15.7515971 com uma precisão de 5 casas decimais.
+SELECT ROUND(15.7515971, 5);
