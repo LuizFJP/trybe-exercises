92e64b928b4225e0791e59899739e741c0b09672
LuizFJP <luizfjportela@gmail.com>
2021-10-20 15:41:32 -0300

    requirement 05

--- Bloco 20/day_01/fixation_exercise01.sql
+++ Bloco 20/day_01/fixation_exercise01.sql
@@ -9,4 +9,6 @@ SELECT CHAR_LENGTH('Uma frase qualquer');
 
 -- Extraia e retorne a palavra "JavaScript" da frase 'A linguagem JavaScript está entre as mais usadas'.
 SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
+
 -- Por fim, padronize a string 'RUA NORTE 1500, SÃO PAULO, BRASIL' para que suas informações estejam todas em caixa baixa.
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
