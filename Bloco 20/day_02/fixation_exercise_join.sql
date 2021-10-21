2a9d6a0b4beace600b4d7b2ffb2aecd5bed49742
LuizFJP <luizfjportela@gmail.com>
2021-10-21 15:37:51 -0300

    exercise 03, 04, 05

--- Bloco 20/day_02/fixation_exercise_join.sql
+++ Bloco 20/day_02/fixation_exercise_join.sql
@@ -9,3 +9,28 @@ SELECT CONCAT(s.first_name,' ',s.last_name) AS Nome, a.address
 FROM staff s
 INNER JOIN address a
 ON s.address_id = a.address_id;
+
+-- Exiba o id do cliente , nome e email dos primeiros 100 clientes, ordenados pelo nome em ordem decrescente, juntamente com o id do endereço e o endereço onde o cliente mora. Essas informações podem ser encontradas nas tabelas customer e address
+SELECT c.customer_id, c.first_name, c.email, a.address_id, a.address
+FROM customer c
+INNER JOIN address a
+ON c.address_id = a.address_id
+ORDER BY c.first_name DESC
+LIMIT 100;
+
+-- Exiba o nome , email , id do endereço , endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes. As informações podem ser encontradas nas tabelas address e customer .
+SELECT c.first_name, c.email, c.address_id, a.address, a.district
+FROM customer c
+INNER JOIN address a 
+ON c.address_id = a.address_id
+WHERE a.district = 'California' AND
+c.first_name LIKE '%rene%';
+
+-- Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por nomes de forma decrescente. Exiba somente os clientes ativos. As informações podem ser encontradas na tabela address e customer .
+SELECT c.first_name, COUNT(a.address_id) AS `Qtd de endereços`
+FROM customer c
+INNER JOIN address a
+ON c.address_id = a.address_id
+WHERE c.active = 1
+GROUP BY c.first_name
+ORDER BY c.first_name DESC;
