82fd828d1ca725892a143b2d23715cf3d214a270
LuizFJP <luizfjportela@gmail.com>
2021-10-21 15:04:46 -0300

    exercise 02

--- Bloco 20/day_02/fixation_exercise_join.sql
+++ Bloco 20/day_02/fixation_exercise_join.sql
@@ -3,3 +3,9 @@ SELECT a.actor_id, a.first_name, f.film_id, f.title
 FROM actor a
 INNER JOIN film f
 ON a.actor_id = f.film_id;
+
+-- Use o JOIN para exibir o nome , sobrenome e endereço de cada um dos funcionários do banco. Use as tabelas staff e address .
+SELECT CONCAT(s.first_name,' ',s.last_name) AS Nome, a.address
+FROM staff s
+INNER JOIN address a
+ON s.address_id = a.address_id;
