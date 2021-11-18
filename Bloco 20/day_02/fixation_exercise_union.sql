843824d5eda78abe73f2eb1f628a9e2226d5132c
LuizFJP <luizfjportela@gmail.com>
2021-11-18 15:50:59 -0300

    fixation exercise 1

--- Bloco 20/day_02/fixation_exercise_union.sql
+++ Bloco 20/day_02/fixation_exercise_union.sql
@@ -7,3 +7,10 @@ SELECT CONCAT(first_name, ' ', last_name) AS Nome FROM (sakila.actor);
 SELECT first_name FROM sakila.customer WHERE first_name LIKE '%tracy%'
 UNION
 SELECT first_name FROM sakila.actor WHERE first_name LIKE '%je%';
+
+-- Monte uma query que exiba a união dos cinco últimos nomes da tabela actor , o primeiro nome da tabela staff e cinco nomes a partir da 15ª posição da tabela customer . Não permita que dados repetidos sejam exibidos. Ordene os resultados em ordem alfabética.
+(SELECT first_name FROM sakila.actor ORDER BY first_name DESC LIMIT 5)
+UNION
+(SELECT first_name FROM sakila.staff LIMIT 1)
+UNION
+(SELECT first_name FROM sakila.customer LIMIT 5 OFFSET 15 ) ORDER BY first_name;
