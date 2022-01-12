c1aa76ffef692812115e3db1db6d7e1bcec47807
LuizFJP <luizfjportela@gmail.com>
2022-01-12 20:00:17 -0300

    fixation-exercise-02

--- Bloco-22-node/day_04/hello-express/index.js
+++ Bloco-22-node/day_04/hello-express/index.js
@@ -10,10 +10,28 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+app.get('/drinks/:id', (req, res) => {
+  const { id } = req.params;
+  const drink = drinks.find((drink) => drink.id === parseInt(id));
+  if (!drink) return res.status(404).json({ message: 'Drink not found!' });
+  res.status(200).json(drink);
+  res.status(200).json(drinks);
+});
+
 app.get('/recipes', function (req, res) {
   res.json(recipes);
 });
 
-app.listen(3002, () => {
-  console.log('Aplicação ouvindo na porta 3002');
+
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
 });
