748328e4b387d706f520fd7eea70bd3d0c340771
LuizFJP <luizfjportela@gmail.com>
2022-04-10 20:36:45 -0300

    feat: create movie controller

---
+++ Bloco-30/day_01/Bookshop/src/controllers/MovieController.ts
@@ -0,0 +1,19 @@
+import MovieService from '../services/MovieService';
+import { Response, Request } from 'express';
+
+class MovieController {
+  constructor(private _movieService = new MovieService()) {}
+
+  public notFound = 'Movie not found';
+  public internalError = 'Internal server error';
+
+  public getMovies = async(req: Request, res: Response): Promise<Response> => {
+    try {
+      const movies = await this._movieService.getMovies();
+
+      return res.status(200).send(movies);
+    } catch (error: unknown) {
+      return res.status(500).send({ message: this.internalError });
+    }
+  }
+}
